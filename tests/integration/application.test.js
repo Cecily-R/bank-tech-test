b32fa3a30d42082636c06ce72157ef2e8ee56246
Cecily Rowland <cec1rowland@gmail.com>
2023-01-26 17:04:05 +0000

    Added integration tests

---
+++ tests/integration/application.test.js
@@ -0,0 +1,36 @@
+const Deposit = require('../../lib/deposit.js')
+const Withdrawal = require('../../lib/withdrawal.js')
+const Account = require('../../lib/account.js')
+const Statement = require('../../lib/statement.js')
+
+beforeAll(() => {
+  jest.useFakeTimers();
+  jest.setSystemTime(new Date(2020, 3, 1))
+});
+
+afterAll(() => {
+  jest.useRealTimers();
+});
+
+describe("application", () => {
+  it("returns a correct statement of withdrawal and deposits", () => {
+    const statement = new Statement();
+    const account = new Account();
+    account.deposit(9000)
+    account.withdraw(456);
+    account.withdraw(250);
+    account.deposit(20.95)
+   
+    const newStatement = statement.formatStatement(account);
+    const statementDate = new Date(2020, 3, 1).toLocaleDateString();
+
+    expect(newStatement).toContain('date || credit || debit || balance');
+    expect(newStatement).toContain(`${statementDate} || 9000.00 ||  || 9000.00`);
+    expect(newStatement).toContain(`${statementDate} ||  || 456.00 || 8544.00`);
+    expect(newStatement).toContain(`${statementDate} ||  || 250.00 || 8294.00`);
+    expect(newStatement).toContain(`${statementDate} || 20.95 ||  || 8314.95`);
+
+    expect(account.accountBalance()).toEqual(8314.95)
+  });
+});
+
--- tests/statement.test.js
+++ tests/statement.test.js
@@ -11,18 +11,43 @@ afterAll(() => {
 });
 
 describe("statement", () => {
-  it("returns all transactions", () => {
+  it("returns an empty statement", () => {
     const statement = new Statement();
     const account = new Account();
-    account.deposit(1296.50);
-    account.withdraw(456);
+
     const newStatement = statement.formatStatement(account);
     const statementDate = new Date(2020, 3, 1).toLocaleDateString();
 
     expect(newStatement).toContain('date || credit || debit || balance');
-    expect(newStatement).toContain(`${statementDate} || 1296.50 ||  || 1296.50`);
-    expect(newStatement).toContain(`${statementDate} ||  || 456.00 || 840.50`);
+    expect(account.accountBalance()).toEqual(0)
 
-    expect(account.accountBalance()).toEqual(840.50)
+  })
+  
+  it("returns a statement including a deposit", () => {
+    const statement = new Statement();
+    const account = new Account();
+    
+    account.deposit(1296.50);
+
+    const newStatement = statement.formatStatement(account);
+    const statementDate = new Date(2020, 3, 1).toLocaleDateString();
+
+    expect(newStatement).toContain('date || credit || debit || balance');
+    expect(newStatement).toContain(`${statementDate} || 1296.50 ||  || 1296.50`)
+    expect(account.accountBalance()).toEqual(1296.50)
+  });
+
+  it("returns a statement including a withdrawal", () => {
+    const statement = new Statement();
+    const account = new Account();
+    
+    account.withdraw(250);
+
+    const newStatement = statement.formatStatement(account);
+    const statementDate = new Date(2020, 3, 1).toLocaleDateString();
+
+    expect(newStatement).toContain('date || credit || debit || balance');
+    expect(newStatement).toContain(`${statementDate} ||  || 250.00 || -250`)
+    expect(account.accountBalance()).toEqual(-250)
   });
 });
